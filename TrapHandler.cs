0c0a1acbae92e5e9540039bd7149bd954b03d627
Nichologeam <nickblanklogan@gmail.com>
2025-08-27 23:36:47 -0700

    handling for new traps in v0.1.3

--- TrapHandler.cs
+++ TrapHandler.cs
@@ -4,6 +4,7 @@ using System;
 using UnityEngine;
 using TMPro;
 using System.Linq;
+using System.Collections;
 
 namespace CUAP;
 
@@ -11,24 +12,44 @@ public class TrapHandler : MonoBehaviour
 {
     public static ApClient Client;
     private Body Vitals;
+    private WorldGeneration worldgen;
 
     private void OnEnable()
     {
         Client = APClientClass.Client;
         Vitals = this.gameObject.GetComponent<Body>();
+        worldgen = GameObject.Find("World").GetComponent<WorldGeneration>();
         Startup.Logger.LogMessage("TrapHandler Ready!");
     }
     public void ProcessTraps(string TrapName)
     {
         if (TrapName == "Depression Trap")
         {
-            Startup.Logger.LogMessage("Happiness lowered.");
             Vitals.happiness =- 20;
         }
         if (TrapName == "Hearing Loss Trap" && Vitals.hearingLoss < 50) // It's a trap item, so let's not lower the player's hearing loss.
         {
-            Startup.Logger.LogMessage("Setting hearing loss to 50.");
             Vitals.hearingLoss = 50;
         }
+        if (TrapName == "Earthquake Trap")
+        {
+            worldgen.earthquakeDelay = 0; // start an earthquake
+            worldgen.earthquakeIntensity = 2; // twice as intense as basegame earthquake
+            worldgen.earthquakeTime = 15; // for 15 seconds
+        }
+        if (TrapName == "Reverse Controls Trap")
+        {
+            StartCoroutine(ReverseControls());
+        }
+        if (TrapName == "Sleep Trap")
+        {
+            Vitals.sleeping = true;
+        }
+    }
+    IEnumerator ReverseControls()
+    {
+        Vitals.reversedControls = true;
+        yield return new WaitForSecondsRealtime(10);
+        Vitals.reversedControls = false;
     }
 }
