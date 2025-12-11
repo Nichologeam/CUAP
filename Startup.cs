0edbe04ab979bfa5a90cc007d0c731d119c26bd5
Nichologeam <nickblanklogan@gmail.com>
2025-12-11 15:43:46 -0800

    disable save files (again)

--- Startup.cs
+++ Startup.cs
@@ -81,7 +81,7 @@ public class Startup : BaseUnityPlugin
             if (Client is null || !(Client?.IsConnected ?? false))
             {
                 Startup.Logger.LogError("Archipelago disconnected mid run! Quitting to main menu to prevent breaking the client!");
-                GameObject.Find("World").GetComponent<WorldGeneration>().SaveAndExit();
+                // GameObject.Find("World").GetComponent<WorldGeneration>().SaveAndExit();
                 PlayerCamera.main.ToMainMenu();
             }
         }
