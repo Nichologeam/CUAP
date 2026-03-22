7d1575a4258f7155884a0e9e1d0ab77fa3e3016f
Nichologeam <nickblanklogan@gmail.com>
2026-03-22 00:56:43 -0700

    guarentee traders sell an AP item

---
+++ TraderInventory.cs
@@ -0,0 +1,22 @@
+﻿using HarmonyLib;
+
+namespace CUAP;
+
+[HarmonyPatch(typeof(TraderScript), "GenerateInventory")]
+// Guarentees an Archipelago item will be inside each trader's inventory
+class TraderInventory
+{
+    static void Postfix(TraderScript __instance)
+    {
+        if (!CraftingChecks.bpLocations)
+        {
+            return; // blueprint locations are disabled
+        }
+        __instance.items.Insert(0, new TraderItem // insert at index 0 to guarentee it's at the top of the list
+        {
+            preference = 0, // always inside the Wants Trade category, making it near the top and cheap
+            id = "blueprint",
+            value = 0 // free
+        });
+    }
+}
