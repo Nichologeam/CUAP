1285becc94ba238ce353d8b6f988afc9100ed965
Nichologeam <nickblanklogan@gmail.com>
2026-03-24 00:27:18 -0700

    num

--- Startup.cs
+++ Startup.cs
@@ -11,12 +11,12 @@ using UnityEngine.UI;
 
 namespace CUAP;
 
-[BepInPlugin("nichologeam.cuap", "Casualties: Unknown Archipelago", "0.7.0.0")]
+[BepInPlugin("nichologeam.cuap", "Casualties: Unknown Archipelago", "0.7.1.0")]
 public class Startup : BaseUnityPlugin
 {
     public static new ManualLogSource Logger;
     public static ArchipelagoSession Client;
-    public static string CUAPVersion = "v0.7.0";
+    public static string CUAPVersion = "v0.7.1";
     public static AssetBundle apassets;
     private static Harmony apHarmony;
     public static Startup instance;
