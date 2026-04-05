6950199d89043fdaffa102f7a27ccee3c317f2c4
Nichologeam <nickblanklogan@gmail.com>
2026-04-04 21:01:01 -0700

    rewrite to make cleaner

--- MinigameLocker.cs
+++ MinigameLocker.cs
@@ -1,4 +1,5 @@
 ﻿using HarmonyLib;
+using System;
 using System.Collections.Generic;
 using UnityEngine;
 
@@ -11,105 +12,50 @@ class MinigameLocker
     private static PlayerCamera plrcam;
     public static bool minigameItemObtained; // only used in one item mode
     public static HashSet<string> minigamesUnlocked = []; // only used in individual mode
+    private static readonly Dictionary<Type, (string itemName, string message)> MinigameMap = new()
+    {
+        { typeof(AmputationMinigame), ("Amputation Minigame", "Amputation is") },
+        { typeof(BandageMinigame), ("Bandage Minigame", "Bandages are") },
+        { typeof(DislocationMinigame), ("Dislocation Minigame", "Fixing dislocations is") },
+        { typeof(KeypadMinigame), ("Keypad Minigame", "Keypads are") },
+        { typeof(LockpingMinigame), ("Lockpicking Minigame", "Lockpicking is") },
+        { typeof(ShrapnelMinigame), ("Shrapnel Minigame", "Removing shrapnel is") },
+        { typeof(SyringeMinigame), ("Syringe Minigame", "Injection is") },
+    };
     static bool Prefix(MinigameBase __instance, Minigame minigame, Item item)
     {
         plrcam = GameObject.Find("Main Camera").GetComponent<PlayerCamera>();
+
         switch (APClientClass.minigameRandom)
         {
             case 1: // disabled
-                return true; // run original function
+                return true; // allow basegame function
             case 2: // one item mode
                 if (minigame is SelfHarmMinigame)
                 {
-                    return true; // always allow Suicide and Self Harm minigames
+                    return true; // always allow Suicide and Self Harm
                 }
                 else if (minigameItemObtained)
                 {
-                    return true; // player has the item. allow original function
+                    return true; // player has the item
                 }
-                else
+                plrcam.DoAlert($"Minigames are locked by {APCanvas.coloredAPText}");
+                return false; // player doesn't have the item
+
+            case 3:
+                if (MinigameMap.TryGetValue(minigame.GetType(), out var data))
                 {
-                    plrcam.DoAlert($"Minigames are locked by {APCanvas.coloredAPText}");
-                    return false; // player does not have the item. don't run function.
+                    if (minigamesUnlocked.Contains(data.itemName))
+                    {
+                        return true; // player has the respective item
+                    }
+                    plrcam.DoAlert($"{data.message} locked by {APCanvas.coloredAPText}");
+                    return false; // player does not
                 }
-            case 3: // individual mode
-                switch (minigame)
-                {
-                    case AmputationMinigame amputation:
-                        if (minigamesUnlocked.Contains("Amputation Minigame"))
-                        {
-                            return true; // player has this minigame
-                        }
-                        else
-                        {
-                            plrcam.DoAlert($"Amputation is locked by {APCanvas.coloredAPText}");
-                            return false; // player does not
-                        }
-                    case BandageMinigame bandage:
-                        if (minigamesUnlocked.Contains("Bandage Minigame"))
-                        {
-                            return true; // player has this minigame
-                        }
-                        else
-                        {
-                            plrcam.DoAlert($"Bandages are locked by {APCanvas.coloredAPText}");
-                            return false; // player does not
-                        }
-                    case DislocationMinigame dislocation:
-                        if (minigamesUnlocked.Contains("Dislocation Minigame"))
-                        {
-                            return true; // player has this minigame
-                        }
-                        else
-                        {
-                            plrcam.DoAlert($"Fixing dislocations is locked by {APCanvas.coloredAPText}");
-                            return false; // player does not
-                        }
-                    case KeypadMinigame keypad:
-                        if (minigamesUnlocked.Contains("Keypad Minigame"))
-                        {
-                            return true; // player has this minigame
-                        }
-                        else
-                        {
-                            plrcam.DoAlert($"Keypads are locked by {APCanvas.coloredAPText}");
-                            return false; // player does not
-                        }
-                    case LockpingMinigame lockpicking: // i have no clue why this is misspelled
-                        if (minigamesUnlocked.Contains("Lockpicking Minigame"))
-                        {
-                            return true; // player has this minigame
-                        }
-                        else
-                        {
-                            plrcam.DoAlert($"Lockpicking is locked by {APCanvas.coloredAPText}");
-                            return false; // player does not
-                        }
-                    case ShrapnelMinigame shrapnel:
-                        if (minigamesUnlocked.Contains("Shrapnel Minigame"))
-                        {
-                            return true; // player has this minigame
-                        }
-                        else
-                        {
-                            plrcam.DoAlert($"Removing shrapnel is locked by {APCanvas.coloredAPText}");
-                            return false; // player does not
-                        }
-                    case SyringeMinigame syringe:
-                        if (minigamesUnlocked.Contains("Syringe Minigame"))
-                        {
-                            return true; // player has this minigame
-                        }
-                        else
-                        {
-                            plrcam.DoAlert($"Injection is locked by {APCanvas.coloredAPText}");
-                            return false; // player does not
-                        }
-                    default: // Suicide or Self Harm. always allow these.
-                        return true; // run original function
-                }
+                return true; // unknown minigame (should only be Suicide and Self Harm)
+
             default: // unset?
-                APCanvas.EnqueueArchipelagoNotification($"minigameRandom is an unhandled value! {APClientClass.minigameRandom}",3);
+                APCanvas.EnqueueArchipelagoNotification($"minigameRandom is an unhandled value! {APClientClass.minigameRandom}", 3);
                 Startup.Logger.LogError($"minigameRandom is an unhandled value! {APClientClass.minigameRandom}");
                 return false;
         }
